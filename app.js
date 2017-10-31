6a9e04084ff296bb2898a6834251d1301448e548
Jordan Mor <jordanmor@gmail.com>
2017-10-31 15:26:38 -0400

    App now gets data from Treehouse API and prints it out to console in pre-designated format

--- app.js
+++ app.js
@@ -1,5 +1,24 @@
+const https = require('https');
+const username = 'jordanmor';
+
 function printMessage(username, badgeCount, point) {
 	const message = `${username} has ${badgeCount} total badge(s) and ${point} points in JavaScript`;
 	console.log(message);
 }
+
+const request = https.get(`https://teamtreehouse.com/${username}.json`, response => {
+	// console.log(response.statusCode);
+
+	let body = '';
+
+	response.on('data', data => {
+		body += data.toString();
+	})
+
+	response.on('end', () => {
+		const profile = JSON.parse(body);
+		printMessage(username, profile.badges.length, profile.points.JavaScript);
+	});
+
+});
 
