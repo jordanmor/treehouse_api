8a5e2fa25a8e9c45cddff51e39543bf5d9e6d522
Jordan Mor <jordanmor@gmail.com>
2017-11-02 16:25:12 -0400

    Add functionality for handling errors

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
 const https = require('https');
+const http = require('http');
+// Print Error Messages
+function printError(error) {
+	console.error(error.message);
+}
 
 function printMessage(name, badgeCount, point) {
 	const message = `${name} has ${badgeCount} total badge(s) and ${point} total points.`;
@@ -7,21 +12,36 @@ function printMessage(name, badgeCount, point) {
 
 function getProfile(username) {
 
-	const request = https.get(`https://teamtreehouse.com/${username}.json`, response => {
-		// console.log(response.statusCode);
+	try {
 
-		let body = '';
+		const request = https.get(`https://teamtreehouse.com/${username}.json`, response => {
+			if (response.statusCode === 200) {
 
-		response.on('data', data => {
-			body += data.toString();
-		})
+				let body = '';
+				// Read the data
+				response.on('data', data => {
+					body += data.toString();
+				})
 
-		response.on('end', () => {
-			const profile = JSON.parse(body);
-			printMessage(profile.name, profile.badges.length, profile.points.total);
+				response.on('end', () => {
+					try {
+						const profile = JSON.parse(body);
+						printMessage(profile.name, profile.badges.length, profile.points.total);
+					} catch (error) {
+						printError(error);
+					}
+				});
+			} else {
+				const message = `There was an error getting the profile for ${username} (${http.STATUS_CODES[response.statusCode]})`;
+				const statusCodeError = new Error(message);
+				printError(statusCodeError);
+			}
 		});
 
-	});
+		request.on('error', error => printError(error));
+	} catch (error) {
+		printError(error);
+	}
 
 }
 const users = process.argv.slice(2);
